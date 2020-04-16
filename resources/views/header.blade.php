83aa5bc2e98c8c9e06bbdd8d2a1dd44bad148889
malisher <d.alisher99@gmail.com>
2020-04-17 03:13:31 +0600

    first commit

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -85,11 +85,34 @@
                 <ul>
                     <li class="search"><img src="sites/all/themes/newtheme/images/search.png" title="Поиск"
                                             alt="Поиск"/></li>
-                    <li class="user-login">
-                        <a href="login.php">
-                            Войти
-                        </a>
-                    </li>
+					@guest
+						<li class="user-login">
+							<a class="nav-link" href="{{ route('login') }}">{{ __('Войти') }}</a>
+						</li>
+						@if (Route::has('register'))
+							<li class="user-login">
+								<a class="nav-link" href="{{ route('register') }}">{{ __('Регистрация') }}</a>
+							</li>
+                        @endif
+					@else
+						<li class="nav-item dropdown">
+							<a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+								{{ Auth::user()->name }} <span class="caret"></span>
+							</a>
+
+							<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+								<a class="dropdown-item" href="{{ route('logout') }}"
+								   onclick="event.preventDefault();
+												 document.getElementById('logout-form').submit();">
+									{{ __('Выход') }}
+								</a>
+
+								<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+									@csrf
+								</form>
+							</div>
+						</li>
+					@endguest
                 </ul>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -30,3 +30,7 @@ Route::get('/', 'HomeViewController@index');
 //});
 
 
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
